db5c139acd604ab203d5e185a7b94f427497bbf9
Ben McMahen <bmcmahen@gmail.com>
2013-01-09 11:55:42 -0700

    beginning work on dynamically generating forms

--- know-eugenics/public/javascripts/forms.js
+++ know-eugenics/public/javascripts/forms.js
@@ -4,8 +4,20 @@
 // is selected, and then be able to parse that form into an object
 // on submission. 
 
-var Formset = function(attributes){
-  var attributes = (attributes || {});
+// API
+var createForm = function(attributes, requiredFields) {
+  attributes = attributes || {};
+  required = requiredFields || [];
+  return new Formset(attributes, required)
+    .determineFields()
+    .render();
+};
+
+
+// Constructor
+var Formset = function(attributes, required){
+  this.attributes = attributes;
+  this.required = required; 
 
   this.fields = {
     title : {widget: 'text'},
@@ -15,39 +27,76 @@ var Formset = function(attributes){
 
 };
 
+// Functions
 _.extend(Formset.prototype, {
 
+  // Cycle through (1) Form Types, (2) App Field Requirements
+  // and extend the field object with each required type
+  // leaving us with one object that contains a list of the required
+  // fields. 
   determineFields: function(required) {
 
-    var self = this; 
-
-    // Cycle through (1) Form Types, (2) App Field Requirements
-    // and extend the field object with each required type
-    // leaving us with one object that contains a list of the required
-    // fields. 
+    var self = this
+      , required = required || this.required; 
     
     _.each(required, function(required){
       _.extend(self.fields, self.requiredFields[required]);
     });
 
-    this.formArray = _.toArray(this.fields);
-
     return this; 
+
   },
 
   requiredFields : {
 
     'event' : {
-      date: { type: 'text', label: 'Date', className:'date' }
+      date: { widget: 'text', label: 'Date', className:'date' }
     },
 
     'timeline' : {
-      date: { type: 'text', label: 'Date', className:'date' },
-      startDate : { type: 'text', label: 'Start Date'},
-      endDate : { type: 'text', label: 'End Date' }
+      date: { widget: 'text', label: 'Date', className:'date' },
+      startDate : { widget: 'text', label: 'Start Date'},
+      endDate : { widget: 'text', label: 'End Date' }
     }
 
+  },
+
+  // If we are editing a document, then we need to add values
+  // to the appropriate field. 
+  addValues : function(){
+
+    var self = this; 
+
+    _.each(this.attributes, function(value, key){
+      self.fields[key].value = value; 
+    });
+
+  },
+
+  // This should eventually use a template system. 
+  render : function() {
+    var html = ''
+      , fieldMap = {
+        'textarea' : '<textarea>a text area</textarea>',
+        'text': '<input type="text">'
+      };
+
+    _.each(this.fields, function(obj, key){
+      var widget = fieldMap[obj.widget];
+      if (typeof widget != 'undefined')
+        html += widget;
+    });
+
+    html += '<input type="submit">';
+    this.el = html;
+    return this; 
   }
-  
 
 });
+
+
+
+var myform = createForm({},['event', 'timeline']);
+var formsetel = document.getElementById('formset');
+formsetel.innerHTML = myform.el;
+console.log(myform);
